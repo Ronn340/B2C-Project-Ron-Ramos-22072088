389dda2bdbf0eabd2d43c93c02b5b4c8d1320b94
Ronn <ronravnr@gmail.com>
2026-05-15 04:14:00 +1000

    (CI pipeline) - removing post remnants

--- apps/web/src/components/Menu/LeftMenu.tsx
+++ apps/web/src/components/Menu/LeftMenu.tsx
@@ -1,44 +1,7 @@
-import { client } from "@repo/db/client";
-import { CategoryList } from "./CategoryList";
-import { HistoryList } from "./HistoryList";
-import { TagList } from "./TagList";
 import Image from "next/image";
 
-
-
-export async function LeftMenu({
-  selectedCategory,
-  selectedYear,
-  selectedMonth,
-  selectedTag,
-}: {
-  selectedCategory?: string; 
-  selectedYear?: string; 
-  selectedMonth?: string
-  selectedTag?: string;
-}) {
-
-  const databasePosts = await client.db.post.findMany({
-    include: { Likes: true }
-  });
-  console.log(databasePosts);
-  const posts = databasePosts.map((post) => ({
-    ...post,
-    likes: post.Likes.length,
-  }));
-
+export async function LeftMenu({}: {}) {
   return (
-    <div className="w-80   h-screen px-10 py-8">
-      {/* Sidebar component, swap this element with another sidebar if you like */}
-      <a className="flex items-center gap-2" href="/">
-        <Image src="/wsulogo.png" alt="WSU Logo" width={32} height={32} />
-        <span className="font-bold text-primary">Full Stack Blog</span>
-      </a>
-      <nav>
-        <CategoryList posts={posts} selectedCategory={selectedCategory} />
-        <HistoryList selectedYear={selectedYear} selectedMonth={selectedMonth} posts={posts} />
-        <TagList selectedTag={selectedTag} posts={posts} />
-      </nav>
-    </div>
+    <div>hello, left menu</div>
   );
 }
