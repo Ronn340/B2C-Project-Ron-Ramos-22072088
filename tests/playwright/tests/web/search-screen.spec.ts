d1bf3b6ad0711bba797ff7c2246a555705ee357e
Ronn <ronravnr@gmail.com>
2026-05-18 23:11:53 +1000

    (test compliance) - seriously half of e2e tests break when i change one static text. like cmon bro

--- tests/playwright/tests/web/search-screen.spec.ts
+++ tests/playwright/tests/web/search-screen.spec.ts
@@ -48,12 +48,12 @@ test.describe("SEARCH SCREEN", () => {
     async ({ page }) => {
       await page.goto("/shop?urlId=abc");
 
-      // SEARCH SCREEN > Displays "0 Posts" when search does not find anything
+      // SEARCH SCREEN > Displays "No items found" when search does not find anything
 
       const articles = await page.locator('[data-test-id^="product-"]');
       await expect(articles).toHaveCount(0);
 
-      await expect(page.getByText("0 Products")).toBeVisible();
+      await expect(page.getByText("No items found")).toBeVisible();
     },
   );
 });
--- tests/playwright/tests/web/type-screen.spec.ts
+++ tests/playwright/tests/web/type-screen.spec.ts
@@ -21,16 +21,14 @@ test.describe("TYPE SCREEN", () => {
 
   test(
     "Invalid Type returns no results",
-    {
-      tag: "@a1",
-    },
+
     async ({ page }) => {
       await page.goto("/shop?type=InvalidType");
 
       const articles = await page.locator('[data-test-id^="product-"]');
       await expect(articles).toHaveCount(0);
 
-      await expect(page.getByText("0 Products")).toBeVisible();
+      await expect(page.getByText("No items found")).toBeVisible();
     },
   );
 });
