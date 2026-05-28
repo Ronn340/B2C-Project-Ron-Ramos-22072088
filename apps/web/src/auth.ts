defd1c517c0062263992a953dbfd6adf0ede113a
Ronn <ronravnr@gmail.com>
2026-05-28 22:11:10 +1000

    (OAuth) - Google sign-in via auth.js

---
+++ apps/web/src/auth.ts
@@ -0,0 +1,13 @@
+import NextAuth, { NextAuthResult } from "next-auth";
+import Google from "next-auth/providers/google";
+
+
+const nextAuth = NextAuth({
+    providers: [Google,]
+});
+
+//My goat JonNode28: Type Inference issue fix "https://github.com/nextauthjs/next-auth/issues/10568#issuecomment-2849090669"
+export const handlers: NextAuthResult["handlers"] = nextAuth.handlers;
+export const signIn: NextAuthResult["signIn"] = nextAuth.signIn;
+export const signOut: NextAuthResult["signOut"] = nextAuth.signOut;
+export const auth: NextAuthResult["auth"] = nextAuth.auth;
