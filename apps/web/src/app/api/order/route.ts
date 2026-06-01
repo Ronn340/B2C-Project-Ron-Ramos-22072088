9f25bd56e590aecee7c321661e42d9ed42562399
Ronn <ronravnr@gmail.com>
2026-06-01 20:31:05 +1000

    (Failed orders) - Loading + invalid success message

--- apps/web/src/app/api/order/route.ts
+++ apps/web/src/app/api/order/route.ts
@@ -1,15 +1,28 @@
 import { NextRequest } from "next/server"
 import { client } from "@repo/db/client";
 import { getCurrentUserId } from "@/utils/auth";
+import { Stripe } from "stripe";
 
 export async function POST(req: NextRequest) {
+    const { stripeSessionId } = await req.json();
+
     //Authentication check
     const userId = await getCurrentUserId();
     if (!userId)
         return Response.json({ message: "Not logged in" }, { status: 401 });
 
+    //Stripe payment paid check
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+    try {
+        const session = await stripe.checkout.sessions.retrieve(stripeSessionId);
+        if (session.payment_status !== "paid") {
+            return Response.json({ message: "Payment not completed" }, { status: 400 });
+        }
+    } catch (error) {
+        return Response.json({ message: "Invalid Stripe session ID" }, { status: 400 });
+    }
+
     //Duplicate orders check
-    const { stripeSessionId } = await req.json();
     const existingOrder = await client.db.order.findUnique({
         where: { stripeSessionId }
     });
@@ -17,18 +30,16 @@ export async function POST(req: NextRequest) {
         return Response.json({ message: "Order already exists for this session" }, { status: 400 });
     }
 
-    //Cart checks (non-existent+empty)
+    //Total amount saved into history
     const cart = await client.db.cart.findUnique({
         where: { userId },
         include: { items: { include: { product: true } } }
     });
 
-    if (!cart)
-        return Response.json({ message: "Cart not found" }, { status: 404 });
-    if (cart.items.length === 0)
+    if (!cart || cart.items.length === 0) {
         return Response.json({ message: "Cart is empty" }, { status: 400 });
+    }
 
-    //Total amount saved into history
     const total = cart.items.reduce((sum, item) => sum + item.quantity * item.product.price, 0);
 
     //Order Creation
@@ -44,7 +55,7 @@ export async function POST(req: NextRequest) {
                     priceAtPurchase: item.product.price,
                     name: item.product.name
                 }))
-            }
+            } //https://www.prisma.io/docs/orm/prisma-client/queries/relation-queries#create-a-related-record
         }
     })
 
--- apps/web/src/app/checkout/success/page.tsx
+++ apps/web/src/app/checkout/success/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { CircleCheckBig } from "lucide-react";
 import { useEffect, useState } from "react";
-import { useSearchParams,useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 
 export default function SuccessPage() {
     const searchParams = useSearchParams();
     const router = useRouter();
     const sessionId = searchParams.get("session_id");
+    const [loading, setLoading] = useState(true)
     const [orderId, setOrderId] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         if (sessionId) {
@@ -18,34 +20,58 @@ export default function SuccessPage() {
                 },
                 body: JSON.stringify({ stripeSessionId: sessionId }),
             })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("Order created:", data);
-                if (data.order?.id) {
-                    setOrderId(data.order.id);
-                }
-            });
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                    if (data.order?.id) {
+                        setOrderId(data.order.id);
+                    } else {
+                        setError(data.message)
+                    }
+                    setLoading(false);
+                });
+        } else {
+            setError("No session ID found");
+            setLoading(false)
         }
     }, [sessionId]);
+
+
+    if (loading) return <div className="text-lg font-semibold flex items-center justify-center h-screen">Loading...</div>
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <div className="flex flex-col items-center max-w-md bg-bg px-10 py-10 rounded-lg shadow-xl">
-                <span><CircleCheckBig className="text-green-500 " size={150} /></span>
-                <div className="flex items-center justify-center mt-5">
-                    <span className="text-3xl font-bold text-primary p-3 border-y border-wsu">Payment Successful!</span>
-                </div>
-                <span className="mt-5 text-secondary">Your order has been placed.</span>
-                <span className="text-center text-secondary">A confirmation email has been sent to your email address.</span>
-                {orderId && <span className="mt-5 text-primary">Order no: {orderId}</span>}
+                {error ?
+                    (
+                        <div className="flex flex-col items-center gap-4">
+                            <span className="text-red-500">{error}</span>
+                            <button
+                                onClick={() => router.push("/")}
+                                className="px-4 py-2 bg-wsu text-bg rounded-full"
+                            >
+                                Go Home
+                            </button>
+                        </div>
+                    ) : (
+                        <>
+                            <span><CircleCheckBig className="text-green-500 " size={150} /></span>
+                            <div className="flex items-center justify-center mt-5">
+                                <span className="text-3xl font-bold text-primary p-3 border-y border-wsu">Payment Successful!</span>
+                            </div>
+                            <span className="mt-5 text-secondary">Your order has been placed.</span>
+                            <span className="text-center text-secondary">A confirmation email has been sent to your email address.</span>
+                            {orderId && <span className="mt-5 text-primary">Order no: {orderId}</span>}
 
-                <div className="flex mt-5">
-                    <button onClick={() => router.push("/")} className="px-4 py-2 bg-wsu text-bg rounded-full hover:bg-textSecondary hover:text-wsu transition-colors ">
-                        Continue Shopping
-                    </button>
-                </div>
-            </div>
-
-        </div>
+                            <div className="flex mt-5">
+                                <button onClick={() => router.push("/")} className="px-4 py-2 bg-wsu text-bg rounded-full hover:bg-textSecondary hover:text-wsu transition-colors ">
+                                    Continue Shopping
+                                </button>
+                            </div>
+                        </>
+                    )
+                }
+            </div >
+        </div >
     )
 }
