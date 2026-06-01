49cdf044309bbe6aec2c3100e879e1eb1f92c8c5
Ronn <ronravnr@gmail.com>
2026-06-01 22:35:08 +1000

    (Sensitive Errors) - error messages giving too much info. e.g. invalid_sessionId

--- apps/web/src/app/checkout/success/page.tsx
+++ apps/web/src/app/checkout/success/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { CircleCheckBig } from "lucide-react";
+import { CircleCheckBig, TriangleAlert } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 
@@ -44,15 +44,20 @@ export default function SuccessPage() {
             <div className="flex flex-col items-center max-w-md bg-bg px-10 py-10 rounded-lg shadow-xl">
                 {error ?
                     (
-                        <div className="flex flex-col items-center gap-4">
-                            <span className="text-red-500">{error}</span>
-                            <button
-                                onClick={() => router.push("/")}
-                                className="px-4 py-2 bg-wsu text-bg rounded-full"
-                            >
-                                Go Home
-                            </button>
-                        </div>
+                        <>
+                            <span><TriangleAlert className="text-red-500 " size={150} /></span>
+                            <div className="flex items-center justify-center mt-5">
+                                <span className="text-3xl font-bold text-primary p-3 border-y border-wsu">Uh oh, Something went wrong!</span>
+                            </div>
+                            <span className="mt-5 text-secondary">Please try again later.</span>
+                            <span className="text-center text-secondary">Contact support if problem persists</span>
+
+                            <div className="flex mt-5">
+                                <button onClick={() => router.push("/")} className="px-4 py-2 bg-wsu text-bg rounded-full hover:bg-textSecondary hover:text-wsu transition-colors ">
+                                    Go Home
+                                </button>
+                            </div>
+                        </>
                     ) : (
                         <>
                             <span><CircleCheckBig className="text-green-500 " size={150} /></span>
