164e508b86ecf6b2a61857285fa378adf72ad396
Ronn <ronravnr@gmail.com>
2026-06-01 19:46:40 +1000

    (Order placement) - Linked the stripe success to api call

--- apps/web/src/app/checkout/success/page.tsx
+++ apps/web/src/app/checkout/success/page.tsx
@@ -1,10 +1,33 @@
 "use client";
 import { CircleCheckBig } from "lucide-react";
-export default function SuccessPage() {
+import { useEffect, useState } from "react";
+import { useSearchParams,useRouter } from "next/navigation";
 
-    function handleHome() {
-        window.location.href = "/";
-    }
+export default function SuccessPage() {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const sessionId = searchParams.get("session_id");
+    const [orderId, setOrderId] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (sessionId) {
+            fetch("/api/order", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ stripeSessionId: sessionId }),
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Order created:", data);
+                if (data.order?.id) {
+                    setOrderId(data.order.id);
+                }
+            });
+        }
+    }, [sessionId]);
+
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <div className="flex flex-col items-center max-w-md bg-bg px-10 py-10 rounded-lg shadow-xl">
@@ -14,10 +37,10 @@ export default function SuccessPage() {
                 </div>
                 <span className="mt-5 text-secondary">Your order has been placed.</span>
                 <span className="text-center text-secondary">A confirmation email has been sent to your email address.</span>
-                <span className="mt-5 text-primary">Order no: #1000432</span>
+                {orderId && <span className="mt-5 text-primary">Order no: {orderId}</span>}
 
                 <div className="flex mt-5">
-                    <button onClick={handleHome} className="px-4 py-2 bg-wsu text-bg rounded-full hover:bg-textSecondary hover:text-wsu transition-colors ">
+                    <button onClick={() => router.push("/")} className="px-4 py-2 bg-wsu text-bg rounded-full hover:bg-textSecondary hover:text-wsu transition-colors ">
                         Continue Shopping
                     </button>
                 </div>
