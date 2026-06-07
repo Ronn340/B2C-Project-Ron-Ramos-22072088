14cf7f24df000686cd6ef6e863022e597df56d86
Ronn <ronravnr@gmail.com>
2026-06-07 15:41:37 +1000

    (test) - productId being sent instead of sizeStockId

--- tests/playwright/tests/web/cart-screen.spec.ts
+++ tests/playwright/tests/web/cart-screen.spec.ts
@@ -62,7 +62,7 @@ test.describe("CART SCREEN", () => {
     test.describe("Update Cart Item Quantity", () => {
         test.beforeEach(async ({ request }) => {
             await request.post("/api/cart", {
-                data: { productId: 2, quantity: 2 },
+                data: { sizeStockId: 2, quantity: 2 },
                 headers: { "Content-Type": "application/json" }
             });
         });
